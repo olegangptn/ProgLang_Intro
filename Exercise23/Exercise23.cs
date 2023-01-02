0119f882216122544d58c13c9109b8b63d22bb12
OLGPTN <oleg_shulaev@mail.ru>
2023-01-02 19:09:44 +0300

    Практическое задание 3

---
+++ Exercise23/Exercise23.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+    
+
+    Console.WriteLine("Введите число");
+    int n = int.Parse(Console.ReadLine());
+    int count = 1;
+
+    while (count <= n)
+    {
+    
+      Console.WriteLine(Math.Pow(count,3));
+      count += 1;
+      
+    }
