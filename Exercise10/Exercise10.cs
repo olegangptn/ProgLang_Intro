c63592e99fede0dae40322e9cdef378b0ad440de
OLGPTN <oleg_shulaev@mail.ru>
2023-01-02 18:12:47 +0300

    Домашнее задание. Семинар 2

---
+++ Exercise10/Exercise10.cs
@@ -0,0 +1,18 @@
+﻿/// Задача 10.  Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+
+
+
+Console.WriteLine ("Введите трехзначное число");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if ((number > 100) && (number < 1000))
+    {
+     number = number % 100 / 10;
+
+    Console.WriteLine(number);
+    }
+    else 
+    {
+      Console.WriteLine("Введено не трехзначное число");
+    }
---
+++ Exercise15/Exercise15.cs
@@ -0,0 +1,21 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+//  и проверяет, является ли этот день выходным.
+
+
+Console.WriteLine("Введите день недели от 1 до 7: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+    if(number >8 || number =0)
+    {
+    Console.WriteLine("Введено некоректное значение");
+    return;
+
+    }
+    if(number >5)
+    {
+    Console.WriteLine("Выходной"); 
+    }
+    else
+    {
+    Console.WriteLine("Будни");
+    }
+    
